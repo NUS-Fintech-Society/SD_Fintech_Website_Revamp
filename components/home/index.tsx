b8253651f0f7bb0a4aa8ab4de2e9cbcdd252d786
softmagnet <zhiwei3141@gmail.com>
2022-07-04 23:17:14 +0800

    Change the partner logos layout to use flexbox properties and make footer responsive

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -14,7 +14,7 @@ const Home = ({ partnersImages, departments }: HomeProps) => {
       <Landing />
       <Milestones />
       <Departments departments={departments} />
-      <Partners {...partnersImages} />
+      <Partners partnersImages={partnersImages} />
     </>
   );
 };
