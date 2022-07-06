b8308bbb4f9d4795371588af830bec17fd010471
Zhiwei <59001819+softmagnet@users.noreply.github.com>
2022-07-06 09:34:02 +0800

    Homepage landing and milestones (#16)
    * transfer code from JiAn's branch and redo milestone with tailwind
    
    * Touch up on homepage

--- components/home/Departments.tsx
+++ components/home/Departments.tsx
@@ -11,22 +11,22 @@ import DepartmentCard from '@components/home/DepartmentCard';
 const Departments = ({ departments }: DepartmentsProps) => {
   return (
     <MaxWidth>
-      <div className="my-4 h-12 text-center text-3xl font-bold">
-        Departments
-      </div>
-      <div className="flex flex-wrap justify-center gap-10">
-        {departments.map(
-          ({ coverImage, cardDescription, name, path }, index) => (
-            <Fragment key={index}>
-              <DepartmentCard
-                coverImage={coverImage}
-                cardDescription={cardDescription}
-                name={name}
-                path={path}
-              />
-            </Fragment>
-          )
-        )}
+      <div className="section-my">
+        <div className=" section-title">Our Departments</div>
+        <div className="flex flex-wrap justify-center gap-10">
+          {departments.map(
+            ({ coverImage, cardDescription, name, path }, index) => (
+              <Fragment key={index}>
+                <DepartmentCard
+                  coverImage={coverImage}
+                  cardDescription={cardDescription}
+                  name={name}
+                  path={path}
+                />
+              </Fragment>
+            )
+          )}
+        </div>
       </div>
     </MaxWidth>
   );
--- components/home/Partners.tsx
+++ components/home/Partners.tsx
@@ -9,24 +9,22 @@ import Image from 'next/image';
 import MaxWidth from '@components/layout/MaxWidth';
 
 const Partners = ({ partnersImages }: PartnersProps) => {
-    return (
-        <MaxWidth>
-            <div className=" mb-10 flex flex-wrap justify-center gap-16">
-                {partnersImages.map(({ src, alt }, index) => {
-                    return (
-                        <Fragment key={index}>
-                            <Image
-                                src={src}
-                                alt={alt}
-                                width="150"
-                                height="150"
-                            />
-                        </Fragment>
-                    );
-                })}
-            </div>
-        </MaxWidth>
-    );
+  return (
+    <MaxWidth>
+      <div className=" section-my ">
+        <div className="section-title ">Our Partners</div>
+        <div className="flex flex-wrap justify-center gap-16">
+          {partnersImages.map(({ src, alt }, index) => {
+            return (
+              <Fragment key={index}>
+                <Image src={src} alt={alt} width="150" height="150" />
+              </Fragment>
+            );
+          })}
+        </div>
+      </div>
+    </MaxWidth>
+  );
 };
 
 export default Partners;
