05e2326b41875b8ff27be7f320140246c27b3816
Nielsencu <nielsencugito@gmail.com>
2022-07-15 22:32:44 +0800

    styled hierarchy and excocards

--- components/exco/hierarchy.tsx
+++ components/exco/hierarchy.tsx
@@ -1,6 +1,7 @@
 import ExcoCard from '@components/exco/excoCards';
 import { Fragment } from 'react';
 import HierarchyProps from '@interfaces/exco/hierarchy';
+import MaxWidth from '@components/layout/MaxWidth';
 
 const Hierarchy = ({categories} : HierarchyProps) => {
     return (
@@ -10,31 +11,34 @@ const Hierarchy = ({categories} : HierarchyProps) => {
             {categories.map(({header, subcategories}) => {
                 return(
                     <Fragment key={header}>
-                        <h1>{header}</h1>
-                        <div>
-                            {subcategories.map(({header, excos}) => {
-                                return (
-                                    <Fragment key={header}>
-                                        <h3>{header}</h3>
-                                        <div>
-                                            {excos.map((exco) => {
-                                                return(
-                                                    <Fragment key={header}>
-                                                        <ExcoCard exco={exco}/>
-                                                    </Fragment>
-                                                )
-                                            })}
-                                        </div>
-                                    </Fragment>
-                                )
-                            })}
+                        <div className='m-28'>
+                            <h1 className='text-5xl font-medium lg:text-6xl'>{header}</h1>
+                            <div className='w-full'>
+                                {subcategories.map(({header, excos}) => {
+                                    return (
+                                        <Fragment key={header}>
+                                            <div className='mb-14'>
+                                                <h2 className='text-4xl font-medium lg:text-5xl'>{header}</h2>
+                                                <div className='mt-14 flex w-full grid-cols-2 gap-20'>
+                                                    {excos.map((exco) => {
+                                                        return(
+                                                            <Fragment key={header}>
+                                                                <ExcoCard exco={exco}/>
+                                                            </Fragment>
+                                                        )
+                                                    })}
+                                                </div>
+                                            </div>
+                                        </Fragment>
+                                        )
+                                })}
+                            </div>
                         </div>
                     </Fragment>
                 );
             })}
             </>
         </div>
-        
         </>
     )
 }
