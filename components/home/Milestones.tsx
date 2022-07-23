8dbfd20c7c8660302308d1e37fff4c35f48aae39
Zzhiwei <zhiwei3141@gmail.com>
2022-07-23 23:58:14 +0800

    Solve error on deployment

--- components/home/Milestones.tsx
+++ components/home/Milestones.tsx
@@ -16,32 +16,52 @@ const Milestones = () => {
             <div className=" w-full  border-b border-stone-800 p-10 text-gray-500 lg:w-1/2 lg:border-r lg:border-b-0">
               <div className="mb-3">
                 <span className="text-h2 text-black">
-                  <CountUp start={memberCount ? 0 : null} end={204} duration={2} redraw={true}>
+                  <CountUp
+                    start={memberCount ? 0 : undefined}
+                    end={204}
+                    duration={2}
+                    redraw={true}
+                  >
                     {({ countUpRef }) => (
-                      <VisibilitySensor onChange={(isVisible: any) => {
-                          if (isVisible) { setMemberCount(true); }
-                      }}>
-                          <span ref={countUpRef} />
+                      <VisibilitySensor
+                        onChange={(isVisible: any) => {
+                          if (isVisible) {
+                            setMemberCount(true);
+                          }
+                        }}
+                      >
+                        <span ref={countUpRef} />
                       </VisibilitySensor>
                     )}
-                  </CountUp>  
-                </span> Members
+                  </CountUp>
+                </span>{' '}
+                Members
               </div>
               <p className="">44% Increase from 2020</p>
             </div>
             <div className="  w-full p-10 text-gray-500 lg:w-1/2">
               <div className="mb-3 ">
                 <span className="text-h2 text-black">
-                  <CountUp start={projectsCount ? 0 : null} end={23} duration={1} redraw={true}>
+                  <CountUp
+                    start={projectsCount ? 0 : undefined}
+                    end={23}
+                    duration={1}
+                    redraw={true}
+                  >
                     {({ countUpRef }) => (
-                      <VisibilitySensor onChange={(isVisible: any) => {
-                          if (isVisible) { setProjectsCount(true); }
-                      }}>
-                          <span ref={countUpRef} />
+                      <VisibilitySensor
+                        onChange={(isVisible: any) => {
+                          if (isVisible) {
+                            setProjectsCount(true);
+                          }
+                        }}
+                      >
+                        <span ref={countUpRef} />
                       </VisibilitySensor>
                     )}
-                  </CountUp>  
-                </span> Projects Completed
+                  </CountUp>
+                </span>{' '}
+                Projects Completed
               </div>
               <p className="">40% YOY growth</p>
             </div>
