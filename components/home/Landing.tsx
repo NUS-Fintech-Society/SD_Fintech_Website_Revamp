6e81e299aa77bbbb325b86ecac7a7511749bf9b2
Zzhiwei <zhiwei3141@gmail.com>
2022-07-30 18:29:26 +0800

    Add graphics to recruitment page

--- components/home/Landing.tsx
+++ components/home/Landing.tsx
@@ -4,7 +4,7 @@ import AnimatedText from 'react-animated-text-content';
 
 const Landing = () => {
   return (
-    <div className="relative h-[calc(100vh-64px)] min-h-[400px] w-screen">
+    <div className="relative h-[calc(60vh-64px)] min-h-[400px] w-screen">
       <div className="absolute -z-10 h-full w-full overflow-hidden">
         <Image
           src="/images/home/landing.jpg"
@@ -15,7 +15,7 @@ const Landing = () => {
         />
       </div>
       <MaxWidth>
-        <div className="flex flex-col gap-y-3 pt-[20vh] text-center">
+        <div className="flex flex-col gap-y-3 pt-[max(100px,_20vh)] text-center">
           <h1 className="text-6xl  font-bold xl:text-7xl">
             NUS Fintech Society
           </h1>
@@ -24,14 +24,15 @@ const Landing = () => {
               Ideate. Innovate. Inspire
             </AnimatedText>
           </div>
-          <div className="my-3 text-xl xl:text-[26px]">
+
+          {/* <div className="my-3 text-xl xl:text-[26px]">
             <b>Passionate &nbsp;&#8212;&nbsp;</b>
             Driving R&D in emerging realms of Blockchain and Machine Learning.
           </div>
           <div className=" text-xl xl:text-[26px]">
             <b>Goal Oriented &nbsp;&#8212;&nbsp;</b>
             Leading more than 200 members with a strong grasp of their fields.
-          </div>
+          </div> */}
         </div>
       </MaxWidth>
     </div>
--- components/recruitment/index.tsx
+++ components/recruitment/index.tsx
@@ -2,12 +2,61 @@ import { RecruitmentProps } from '@interfaces/recruitment';
 import Landing from '@components/recruitment/Landing';
 import WhoShouldJoin from '@components/recruitment/WhoShouldJoin';
 import Faq from '@components/recruitment/Faq';
+import MaxWidth from '@components/layout/MaxWidth';
+import Image from 'next/image';
 
 const Recruitment = ({ departmentFaq }: RecruitmentProps) => {
   return (
     <>
       <Landing />
+      <MaxWidth>
+        <div className="section-my pb-0 ">
+          <h2 className=" mb-10 text-center text-h2 font-bold">
+            NUS Fintech Society Recruitment for AY 2022/2023
+          </h2>
+          <p className=" text-center text-xl lg:text-2xl">
+            At NUS Fintech Society, we work on industry-grade financial digital
+            transformation projects, carry out research projects and educate
+            students with Fintech knowledge. From blockchain developer to
+            software engineer to project management executive, we have both
+            technical and operational roles for all who would like to join us.
+          </p>
+          <div className="flex flex-wrap">
+            <div className="md:w-1/2 ">
+              <div className="flex justify-center">
+                <Image
+                  src="/images/recruitment/graphic1.jpg"
+                  alt="graphic 1"
+                  height={280}
+                  width={280}
+                />
+              </div>
+              <p className="mb-3 text-center text-xl font-bold">Technology</p>
+              <p className="mx-auto text-center lg:w-2/3">
+                Learn more about machine learning, blockchain or software
+                development through being involved in industry-related projects{' '}
+              </p>
+            </div>
+            <div className="md:w-1/2">
+              <div className="flex justify-center">
+                <Image
+                  src="/images/recruitment/graphic2.jpg"
+                  alt="graphic 1"
+                  height={280}
+                  width={280}
+                />
+              </div>
+              <p className="mb-3 text-center text-xl font-bold">Operations </p>
+              <p className="mx-auto text-center lg:w-2/3">
+                Experience planning for large-scale events and build close
+                relationships with industry partners
+              </p>
+            </div>
+          </div>
+        </div>
+      </MaxWidth>
       <WhoShouldJoin />
+
       <Faq departmentFaq={departmentFaq} />
     </>
   );
