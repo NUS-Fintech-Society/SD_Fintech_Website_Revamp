dc2397c861632aa20077c35badfb7ccfec5dde0d
Hung Tien <leehungtien@gmail.com>
2022-10-11 23:46:29 +0800

    Added border for image

--- components/home/Landing.tsx
+++ components/home/Landing.tsx
@@ -5,13 +5,14 @@ import AnimatedText from 'react-animated-text-content';
 const Landing = () => {
   return (
     <div className="relative h-[calc(60vh-64px)] min-h-[400px] w-screen">
-      <div className="absolute -z-10 h-full w-full overflow-hidden">
+      <div className="absolute -z-10 h-5/6 w-full overflow-hidden bg-primary">
         <Image
           src="/images/home/home_img.jpg"
           alt="Insert Photo Here"
           layout="fill"
           objectFit="cover"
           quality={100}
+          className="rounded-br-full shadow"
         />
       </div>
       <MaxWidth>
@@ -24,17 +25,17 @@ const Landing = () => {
               Ideate. Innovate. Inspire
             </AnimatedText>
           </div>
-
-          {/* <div className="my-3 text-xl xl:text-[26px]">
-            <b>Passionate &nbsp;&#8212;&nbsp;</b>
-            Driving R&D in emerging realms of Blockchain and Machine Learning.
-          </div>
-          <div className=" text-xl xl:text-[26px]">
+          {/* <div className=" text-xl xl:text-[26px]">
             <b>Goal Oriented &nbsp;&#8212;&nbsp;</b>
             Leading more than 200 members with a strong grasp of their fields.
           </div> */}
         </div>
       </MaxWidth>
+      <div className='absolute bottom-8 w-full  bg-primary xl:bottom-8'>
+        <div className="my-3 text-base italic text-white sm:ml-10 xl:text-[20px] ">
+          Driving R&D in emerging realms of Blockchain and Machine Learning.
+        </div>
+      </div>
     </div>
   );
 };
