18340ce59d6896fa4883476c280f40996eaae314
Gerald <102348985+geraldngjx@users.noreply.github.com>
2022-12-08 01:39:09 +0800

    updated FeaturedProjects and ProjectCard file

--- components/departments/FeaturedProjects.tsx
+++ components/departments/FeaturedProjects.tsx
@@ -7,53 +7,41 @@ import Image from 'next/image';
 
 // code
 import MaxWidth from '@components/layout/MaxWidth';
-import ProjectContent from '@components/departments/ProjectContent';
-import CustomAccordion from '@components/recruitment/CustomAccordion';
 import ProjectCard from '@components/departments/ProjectCard';
+import { arrayBuffer } from 'stream/consumers';
 
 const FeaturedProjects = ({ projects }: FeaturedProjectsProps) => {
   // eslint-disable-next-line no-unused-vars
   const [year, setYear] = useState('21/22');
   const projectsByYear = projects.filter((project) => project.year === year);
-
   // const handleChangeYear = (event: React.ChangeEvent<HTMLSelectElement>) => {
   //   setYear(event.target.value);
   // };
 
   return (
-    
     <MaxWidth>
-    <h3 className='absolute m-12 mt-[60px] hidden md:block md:ml-[475px] lg:ml-[710px] xl:ml-[850px]  text-xl text-white max-w-lg '>Our projects focus on solving problems that impact real people for internal use and industry partnerships</h3>
-    <div className='w-full flex flex-wrap rounded-[20px] p-8 mb-8 '>
-      <div className='w-full h-[122px] flex flex-wrap bg-[#004F5080] rounded-[20px]'>
-        <img src='/images/home/partnerLogos/ourpartners.png' className= 'w-full h-full rounded-[20px]' />
+      {/* <div className='w-full h-[122px] flex flex-wrap bg-[#004F5080] rounded-[20px]'>
+        <img src='/images/home/partnerLogos/bybit.png' className= 'w-full h-full rounded-[20px]' />
+      </div> */}
+      <div className="flex w-full flex-wrap rounded-[10px] bg-black p-3 sm:rounded-[20px] sm:p-6">
+        <h2 className="text-center font-bold text-white sm:text-3xl">
+          Featured Projects
+        </h2>
+        <h3 className="mt-1 ml-auto max-w-lg text-right text-xs text-white sm:text-lg">
+          Key Highlights
+        </h3>
       </div>
-      <div className='absolute flex m-[70px] mt-[40px] text-4xl text-center sm:text-5xl sm:mt-8 font-bold text-white'>
-        Featured Projects
-      </div>
-    </div>
-      <div className="mt-4 xl:mx-20 flex flex-wrap justify-center gap-16">
-      {projectsByYear.map(
-            (
-              {
-                projectName,
-                htmlContent,
-                projectImage,
-                members,
-                collaborations,
-              },
-              index
-            ) => {
-              return (
-                <ProjectCard
+      <div className="mt-4 flex flex-wrap justify-center gap-16 xl:mx-20">
+        {projectsByYear.map(({ projectName, summary, projectImage }, index) => {
+          return (
+            <ProjectCard
               key={index}
               coverImage={projectImage[0]}
-              cardDescription={htmlContent}
+              cardDescription={summary}
               name={projectName}
             />
-              );
-            }
-          )}
+          );
+        })}
       </div>
     </MaxWidth>
   );
--- components/departments/ProjectCard.tsx
+++ components/departments/ProjectCard.tsx
@@ -4,7 +4,6 @@ import { ProjectCardProps } from '@interfaces/departments/ProjectCard';
 // library
 import Link from 'next/link';
 import Image from 'next/image';
-import { WingType } from '@data/homeData';
 
 const ProjectCard = ({
   coverImage,
@@ -12,32 +11,28 @@ const ProjectCard = ({
   name,
 }: ProjectCardProps) => {
   return (
-    <Link href={`/departments/`}>
-      <a className="flex basis-[350px] flex-col rounded-[20px] shadow-[0_2px_30px_3px_rgba(0,0,0,0.1)] mb-10 w-20">
-        <div className="flex justify-center">
+    <div>
+      <a className="flex basis-[350px] flex-col rounded-[20px] shadow-[0_2px_30px_3px_rgba(0,0,0,0.1)] mt-10 mb-10 w-[347px] h-[498px]">
+        <div className="relative overflow-hidden w-[350px] h-[205px] rounded-t-[20px] mb-3 flex justify-center">
           <Image
             src={coverImage.src}
             alt={coverImage.alt}
-            height={180}
-            width={180}
-            layout="intrinsic"
+            layout="fill"
           />
         </div>
         <div className="grow rounded-b-lg pl-6 min-h-[256px]">
-          <div className='min-h-[92px] items-center my-4 mr-5'>
-            <h3 className={`text-[#004C98] text-3xl font-semibold`}>{name}</h3>
+          <div className='min-h-[92px] grid content-center my-2 mr-5'>
+            <h3 className={`text-[#004C98] text-2xl font-bold`}>{name}</h3>
           </div>
-          <div className="text-[#303030] font-medium mr-5 mb-8 leading-5 overflow-hidden" dangerouslySetInnerHTML={{
-            __html: cardDescription,
-          }}/>
+          <div className="max-h-[150px] w-[304px] text-[#303030] text-base font-medium mr-5 mb-8 leading-5 overflow-hidden">{cardDescription}</div>
         </div>
         <div className="flex space-x-2 justify-left">
-          <div>
-            <button type="button" className="inline-block m-5 px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Read More</button>
-          </div>
+          <Link href={`/departments/`}>
+            <button type="button" className="inline-block w-[131px] m-5 px-6 py-2.5 bg-[#004080] rounded-xl text-white font-medium text-xs leading-tight uppercase shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Read More</button>
+          </Link>
         </div>
       </a>
-    </Link>
+    </div>
   );
 };
 
