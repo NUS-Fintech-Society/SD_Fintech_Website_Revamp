be672b935cbd8615f5db9dba850411eee72db337
Gerald <102348985+geraldngjx@users.noreply.github.com>
2023-02-11 14:45:02 +0800

    populated events file to test carousel feature

--- data/eventsData.ts
+++ data/eventsData.ts
@@ -13,6 +13,72 @@ const eventsData: EventsData = {
       location: 'Online',
       date: new Date(2022, 7, 6).toString(),
     },
+    {
+      name: 'Welcome Tea',
+      coverImage: {
+        src: 'https://images.unsplash.com/photo-1595206133361-b1fe343e5e23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80',
+        alt: 'Welcome Image',
+      },
+      cardDescription:
+        'Join us at our virtual Welcome Tea information sharing session to find out more about what we do and who we are looking for!',
+      location: 'Online',
+      date: new Date(2022, 7, 6).toString(),
+    },
+    {
+      name: 'Welcome Tea',
+      coverImage: {
+        src: 'https://images.unsplash.com/photo-1595206133361-b1fe343e5e23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80',
+        alt: 'Welcome Image',
+      },
+      cardDescription:
+        'Join us at our virtual Welcome Tea information sharing session to find out more about what we do and who we are looking for!',
+      location: 'Online',
+      date: new Date(2022, 7, 6).toString(),
+    },
+    {
+      name: 'Welcome Tea',
+      coverImage: {
+        src: 'https://images.unsplash.com/photo-1595206133361-b1fe343e5e23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80',
+        alt: 'Welcome Image',
+      },
+      cardDescription:
+        'Join us at our virtual Welcome Tea information sharing session to find out more about what we do and who we are looking for!',
+      location: 'Online',
+      date: new Date(2022, 7, 6).toString(),
+    },
+    {
+      name: 'Welcome Tea',
+      coverImage: {
+        src: 'https://images.unsplash.com/photo-1595206133361-b1fe343e5e23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80',
+        alt: 'Welcome Image',
+      },
+      cardDescription:
+        'Join us at our virtual Welcome Tea information sharing session to find out more about what we do and who we are looking for!',
+      location: 'Online',
+      date: new Date(2022, 7, 6).toString(),
+    },
+    {
+      name: 'Welcome Tea',
+      coverImage: {
+        src: 'https://images.unsplash.com/photo-1595206133361-b1fe343e5e23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80',
+        alt: 'Welcome Image',
+      },
+      cardDescription:
+        'Join us at our virtual Welcome Tea information sharing session to find out more about what we do and who we are looking for!',
+      location: 'Online',
+      date: new Date(2022, 7, 6).toString(),
+    },
+    {
+      name: 'Welcome Tea',
+      coverImage: {
+        src: 'https://images.unsplash.com/photo-1595206133361-b1fe343e5e23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80',
+        alt: 'Welcome Image',
+      },
+      cardDescription:
+        'Join us at our virtual Welcome Tea information sharing session to find out more about what we do and who we are looking for!',
+      location: 'Online',
+      date: new Date(2022, 7, 6).toString(),
+    },
   ],
 };
 
