373f59895f93ede61327a51d81554ce6389fbf8c
Hung Tien <leehungtien@gmail.com>
2023-03-15 17:12:20 +0800

    Added read more functionality

--- components/events/OtherEvents.tsx
+++ components/events/OtherEvents.tsx
@@ -2,7 +2,7 @@
 import { EventsProps } from '@interfaces/events/Events';
 
 // library
-import { Fragment, useState } from 'react';
+import { Fragment, useState, useEffect, useRef } from 'react';
 import SectionHeader from '@components/layout/SectionHeader';
 import Image from 'next/image';
 
@@ -12,8 +12,21 @@ import EventCard from '@components/events/EventCard';
 import PastEventCard from './PastEventCard';
 
 function ExpandPastEventCard() {
+  const [isReadMore, setIsReadMore] = useState(false);
+  // const readMoreRef = useRef(null);
+
+  // useEffect(() => {
+  //   function handleClickOutside(event) {
+  //     if (readMoreRef.current && !readMoreRef.current.contains(event.target)) {
+  //       setIsReadMore(false);
+  //     }
+  //   }
+
+  //   window.addEventListener('click', handleClickOutside);
+  //   return () => window.removeEventListener('click', handleClickOutside);
+  // }, [readMoreRef]);
   return (
-    <div className="relative h-[680px] w-[1200px] rounded-2xl p-8">
+    <div className="relative h-[680px] w-[1200px] rounded-2xl bg-[#004C98]/50 p-8">
       <Image
         src="/images/events/event_image.jpg"
         alt=""
@@ -21,21 +34,50 @@ function ExpandPastEventCard() {
         objectFit="cover"
         className="-z-10 rounded-2xl shadow-lg"
       />
-      <div className='absolute bottom-5'>
-        <h2 className="font-bold text-white sm:text-6xl">Fintech Event 2020</h2>
-        <h3 className="mt-6 text-xs text-white sm:text-2xl">
-          This was an event that was held in the year 2020 about so and so...
-        </h3>
-        <button className='mt-6 outline outline-offset-2 outline-white-500 text-white font-bold py-2 px-4 rounded-full'>
-          Read More...
-        </button>
-      </div>
+      {isReadMore ? (
+        <div className='mt-12'>
+          <h2 className="font-bold text-white sm:text-5xl">Fintech Event 2020</h2>
+          <h3 className="mt-6 text-xs text-white sm:text-2xl">
+          This was an event that was held in the year 2020 along with Venmo.
+          We had our first ever virtual Welcome Tea information sharing session 
+          to find out more about what we do and who we are looking for !
+          </h3>
+        </div>
+      ) : (
+        <div className='absolute bottom-5'>
+          <h2 className="font-bold text-white sm:text-6xl">Fintech Event 2020</h2>
+          <h3 className="mt-6 text-xs text-white sm:text-2xl">
+            This was an event that was held in the year 2020 about so and so...
+          </h3>
+          <div>
+            <button className='mt-6 outline outline-offset-2 outline-white-500 text-white
+              font-bold py-2 px-4 rounded-full'
+              onClick={() => setIsReadMore(true)}>
+              Read More...
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
 
 const OtherEvents = ({ events }: EventsProps) => {
   const [isShown, setIsShown] = useState(false);
+
+  const expandedCardRef = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (expandedCardRef.current && !expandedCardRef.current.contains(event.target)) {
+        setIsShown(false);
+      }
+    }
+
+    window.addEventListener('click', handleClickOutside);
+    return () => window.removeEventListener('click', handleClickOutside);
+  }, [expandedCardRef]);
+
   return (
     <div>
       <MaxWidth>
@@ -69,25 +111,26 @@ const OtherEvents = ({ events }: EventsProps) => {
 
       <div>
         <div className="mt-6 ml-8 grid grid-cols-2 gap-4 sm:ml-10 lg:grid-cols-3">
-          <div onClick={() => setIsShown(current => !current)}>
+          <div onClick={() => setIsShown(true)}>
             <PastEventCard />
           </div>
-          <div onClick={() => setIsShown(current => !current)}>
+          <div onClick={() => setIsShown(true)}>
             <PastEventCard />
           </div>
-          <div onClick={() => setIsShown(current => !current)}>
+          <div onClick={() => setIsShown(true)}>
             <PastEventCard />
           </div>
-          <div onClick={() => setIsShown(current => !current)}>
+          <div onClick={() => setIsShown(true)}>
             <PastEventCard />
           </div>
-          <div onClick={() => setIsShown(current => !current)}>
+          <div onClick={() => setIsShown(true)}>
             <PastEventCard />
           </div>
-          <div onClick={() => setIsShown(current => !current)}>
+          <div onClick={() => setIsShown(true)}>
             <PastEventCard />
           </div>
-          <div className='absolute z-20' onClick={() => setIsShown(current => !current)}>
+          {/* <div className='absolute z-20' onClick={() => setIsShown(current => !current)}> */}
+          <div className='absolute z-20'>
             {isShown && <ExpandPastEventCard />}
           </div>
         </div>
