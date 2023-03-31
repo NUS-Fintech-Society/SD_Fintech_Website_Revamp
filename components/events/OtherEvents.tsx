8620fc53e4816864158cd9aa15695a7d8751e30c
Hung Tien <leehungtien@gmail.com>
2023-03-31 12:07:33 +0800

    Added button

--- components/events/OtherEvents.tsx
+++ components/events/OtherEvents.tsx
@@ -48,6 +48,10 @@ function ExpandPastEventCard() {
             session to find out more about what we do and who we are looking for
             !
           </h3>
+          <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <svg aria-hidden="true" className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            <span className="sr-only">Icon description</span>
+          </button>
         </div>
       ) : (
         <div className="absolute bottom-5">
