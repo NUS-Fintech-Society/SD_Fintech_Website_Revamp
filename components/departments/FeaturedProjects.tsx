82ed22df58f68acb2b8bd33fcd95e765fbbf5bf4
Gerald <102348985+geraldngjx@users.noreply.github.com>
2023-04-02 16:07:28 +0800

    Add files via upload

--- components/departments/FeaturedProjects.tsx
+++ components/departments/FeaturedProjects.tsx
@@ -7,9 +7,11 @@ import React, { useState } from 'react';
 // code
 import ProjectCard from '@components/departments/ProjectCard';
 import SectionHeader from '@components/layout/SectionHeader';
+import DepartmentFilter from '@components/departments/DepartmentFilter';
 
 const FeaturedProjects = ({ projects }: FeaturedProjectsProps) => {
   // eslint-disable-next-line no-unused-vars
+  const [active, setActive] = useState(false);
   const [year, setYear] = useState('21/22');
   const projectsByYear = projects.filter((project) => project.year === year);
   const projectArray = [];
@@ -18,7 +20,7 @@ const FeaturedProjects = ({ projects }: FeaturedProjectsProps) => {
     const chunk = projectsByYear.slice(i, i + chunkSize);
     projectArray[projectArray.length] = chunk;
   }
-  return (
+  return ( 
     <div className="sm:section-my mt-10">
       <SectionHeader
         color="green"
@@ -26,6 +28,8 @@ const FeaturedProjects = ({ projects }: FeaturedProjectsProps) => {
         subtitle="Key Highlights"
       />
 
+      <button type="submit" onClick={() => setActive(!active)} className="inter mt-10 ml-20 rounded bg-[#036167] py-2 px-4 text-base font-semibold text-white">ACADEMIC YEAR</button>
+      {active === true && <DepartmentFilter/>}
       {/* Mobile */}
       <div className="mt-10 flex flex-wrap justify-center gap-10 md:mt-14 md:gap-16 lg:hidden">
         {projectsByYear.map(({ projectName, summary, projectImage }, index) => {
