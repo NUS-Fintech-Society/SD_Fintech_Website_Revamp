50228d495940617c943f89cf7a3c5df0669a6c00
Ritika Joshi <ritikajoshi0114@gmail.com>
2023-06-05 21:14:14 +0800

    Update mock data and role interface

--- data/recruitmentSubpageData.ts
+++ data/recruitmentSubpageData.ts
@@ -3,28 +3,26 @@ import RecruitmentSubpageData from '@data/interface/recruitmentSubpageData';
 const recruitmentSubpageData: RecruitmentSubpageData = {
     roles: [
         {
-            roleName: 'Software Engineer',
-            aboutUs: 'insert text',
-            onboarding: 'insert text',
-            requirements: 'insert text',
-        }, 
+            name: 'Software Engineer',
+            path: 'software-engineer',
+            logo: {
+                src: '/images/departments/departmentLogos/SD-main-icon.jpg',
+                alt: 'SD logo',
+            },
+            aboutUs: 'Once upon a time, in the distant land of Veridia, a magnificent kingdom stood tall, surrounded by lush green valleys and majestic mountains. The kingdom was known for its vibrant culture, thriving economy, and wise rulers who governed with fairness and justice.',
+            onboarding: 'Once upon a time, in the distant land of Veridia, a magnificent kingdom stood tall, surrounded by lush green valleys and majestic mountains. The kingdom was known for its vibrant culture, thriving economy, and wise rulers who governed with fairness and justice.',
+            requirements: 'Once upon a time, in the distant land of Veridia, a magnificent kingdom stood tall, surrounded by lush green valleys and majestic mountains. The kingdom was known for its vibrant culture, thriving economy, and wise rulers who governed with fairness and justice.',
+        },
         {
-            roleName: 'Technical Lead',
+            name: 'Technical Lead',
+            path: 'technical-lead',
+            logo: {
+                src: '/images/departments/departmentLogos/SD-main-icon.jpg',
+                alt: 'SD logo',
+            },
             aboutUs: 'insert text',
             onboarding: 'insert text',
             requirements: 'insert text',
-        }, 
-        {
-            roleName: 'Design Manager',
-            aboutUs: 'insert text',
-            onboarding: 'insert text',
-            requirements: 'insert text',
-        }, 
-        {
-            roleName: 'UI/UX Designer',
-            aboutUs: 'insert text',
-            onboarding: 'insert text',
-            requirements: 'insert text',
-        }, 
+        },
     ]
 }
