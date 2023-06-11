4e41cfb12872816e8e51135995690a1ab4c7bd2d
triciiaaa <angjeanee@gmail.com>
2023-06-12 02:26:45 +0800

    updated roles text styling

--- components/roles/RolesText.tsx
+++ components/roles/RolesText.tsx
@@ -4,7 +4,7 @@ import { RolesTextProps } from '@interfaces/roles/RolesText';
 const RolesText = ({ header, desc }: RolesTextProps) => {
   return (
     <>
-      <h3 className="mb-5 text-h3 font-bold text-[#002750]">
+      <h3 className="mb-5 text-xl font-bold lg:text-3xl text-[#002750]">
         {header}
       </h3>
       <p className="text-l mb-10 lg:text-xl text-[#002750]">
