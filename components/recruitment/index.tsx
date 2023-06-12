7fa57e2ba4c7ee92b698a49a353dcadc8c8c1c8c
zendenleong <e0726553@u.nus.edu>
2023-06-12 11:03:25 +0800

    tech wing & op wing pages + edited nav bar

--- components/recruitment/index.tsx
+++ components/recruitment/index.tsx
@@ -6,11 +6,20 @@ import MaxWidth from '@components/layout/MaxWidth';
 import Image from 'next/image';
 
 
+import React,{useState} from "react";
+import { Tabs, TabList, Tab } from '@chakra-ui/react';
+
+
 
 const Recruitment = ({ departmentFaq }: RecruitmentProps) => {
+
+  
+  const [isInternal, setIsInternal] = useState(true);
+
   return (
     <>
     <MaxWidth>
+      <div className='flex'>
         <div className="mt-3 flex flex-wrap text-[24.48px] font-bold leading-8 text-[#002750] ">
           <div className="relative w-[47.46px] h-[52px]  sm:w-[91.27px] sm:h-[100px]">
             <Image src='/images/recruitment/Recruitment-logo.jpg' alt='Recruitment Logo' layout="fill" />
@@ -19,6 +28,18 @@ const Recruitment = ({ departmentFaq }: RecruitmentProps) => {
             Recruitment
           </h1>
         </div>
+
+        <div className="hidden xl:block xl:mt-16 xl:ml-auto">
+          <div className = "rounded-md w-80 ml-auto mr-10 bg-white border-2 shadow-lg">
+            <Tabs color='grey' size='sm' padding='1' onChange={() => setIsInternal(!isInternal)} >
+              <TabList>
+                <Tab className="flex-1" _selected={{ color: "#090071", bg:"#EDECF6", fontWeight:"bold"}}>Technical Wing</Tab>
+                <Tab className="flex-1" _selected={{ color: "#090071",bg:"#EDECF6",  fontWeight:"bold" }}>Operation Wing</Tab>
+              </TabList>
+            </Tabs>
+          </div>
+        </div>
+      </div>
 
 
         <div className="section-my pb-0">
@@ -39,7 +60,7 @@ const Recruitment = ({ departmentFaq }: RecruitmentProps) => {
           </h2>
 
           <p className=" text-left text-sm text-[#002750] sm:text-xl">
-            The NUS FinTech Society is looking for <strong>passionate and driven people</strong> 
+            The NUS FinTech Society is looking for <strong> passionate and driven people </strong> 
             looking to be at the forefront of cutting edge technology in the
             FinTech space. Learners of <strong>all skill levels are welcome</strong> and interested
             parties can discover more from the <strong>roles listed below.</strong>
