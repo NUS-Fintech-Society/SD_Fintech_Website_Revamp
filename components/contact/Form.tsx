3ce586b60542162b955705a4b4ceb29b150099b0
Ritika Joshi <ritikajoshi0114@gmail.com>
2023-07-20 15:45:44 +0800

    Improve form validation

--- components/contact/Form.tsx
+++ components/contact/Form.tsx
@@ -1,14 +1,47 @@
-import { FormLabel, Input, Select, Stack } from '@chakra-ui/react';
+import { FormControl, FormErrorMessage, FormLabel, Input, Select, Stack, Textarea } from '@chakra-ui/react';
 import rolesData from '@data/rolesData';
 import departmentsData from '@data/departmentsData';
 import { DepartmentsProps } from '@interfaces/departments';
 import { RoleProps } from '@interfaces/roles';
+import { useState } from 'react';
 
+const initValues = { fname: '', email: '', message: '' };
+const initTouched = {fname: false, email: false, message: false}
+const containerColour = '#f5f5f7'
+const inputContainerWidth = ['280px', '280px', '480px', '600px']
+const inputContainerHeight = ['30px', '30px', '40px', '45px']
+
+
+const initInput = { values: initValues }
+const initTouch = { touched: initTouched}
 const Form = () => {
 
-  const containerColour = '#f5f5f7'
-  const inputContainerWidth = ['280px', '280px', '480px', '600px']
-  const inputContainerHeight = ['30px', '30px', '40px', '45px']
+  const [input, setInput] = useState(initInput)
+  const [isTouched, setIsTouched] = useState(initTouch)
+
+  const { values } = input
+  const { touched } = isTouched
+
+  const onBlur = ({ target }: any) =>
+    setIsTouched(prev => ({
+      ...prev,
+      touched: {
+        ...prev.touched,
+        [target.name]: true
+      }
+
+    }))
+
+  const handleChange = ({ target }: any) =>
+    setInput(prev => ({
+      ...prev,
+      values: {
+        ...prev.values,
+        [target.name]: target.value
+      }
+    }))
+
+
 
   return (
     <div className="m-2 md:m-5 lg:mt-0 flex flex-col">
@@ -18,78 +51,53 @@ const Form = () => {
 
       <div className="mt-5 flex flex-col items-center">
         <form
-          action="https://formsubmit.co/120d98fc78eb82935edd63ceac369850"
+          action="https://formsubmit.co/57ba0ba7fab2eeecf5e10de6f0388f08"
           method="POST"
         >
+          <Input type="hidden" name="_template" value="table" />
+          <Input type="hidden" name="_subject" value="Enquiry for NUS Fintech Society" />
+          {/* Replace with domain name */}
+          <Input type="hidden" name="_next" value="http://localhost:3000/contact-us" />
 
-          <Stack spacing={[5,5,6]} marginBottom={[5,5,6]}>
-            <Input type='text' variant='filled' placeholder='Full Name' bg={containerColour} boxShadow='md'w={inputContainerWidth} h={inputContainerHeight}/>
-            <Input type='email' variant='filled' placeholder='Email Address' bg={containerColour} boxShadow='md' w={inputContainerWidth} h={inputContainerHeight}/>
-            <Select variant='filled' placeholder='Role' bg={containerColour} boxShadow='md' w={inputContainerWidth} h={inputContainerHeight}>
+          <Stack spacing={[5, 5, 6]} marginBottom={[5, 5, 6]}>
+            <FormControl isRequired isInvalid={touched.fname}>
+              <Input
+                name='Name'
+                value={values.fname}
+                onChange={handleChange}
+                onBlur={onBlur}
+                variant='filled'
+                placeholder='Full Name'
+                bg={containerColour}
+                boxShadow='md'
+                w={inputContainerWidth}
+                h={inputContainerHeight} />
+              <FormErrorMessage>Email is required.</FormErrorMessage>
+            </FormControl>
+
+            <FormControl isRequired>
+              <Input name='Email' value={values.email} onChange={handleChange} type='email' variant='filled' placeholder='Email Address' bg={containerColour} boxShadow='md' w={inputContainerWidth} h={inputContainerHeight} />
+            </FormControl>
+
+            <Select name='Role' variant='filled' placeholder='Role' bg={containerColour} boxShadow='md' w={inputContainerWidth} h={inputContainerHeight}>
               <option value='NA'>Not Applicable</option>
               {rolesData.roles.map((role: RoleProps, index: any) =>
                 <option value={role.name} key={index}>{role.name}</option>
               )}
             </Select>
-            <Select variant='filled' placeholder='Relevant Department' bg={containerColour} boxShadow='md' w={inputContainerWidth} h={inputContainerHeight}>
+
+            <Select name='Department' variant='filled' placeholder='Relevant Department' bg={containerColour} boxShadow='md' w={inputContainerWidth} h={inputContainerHeight}>
               <option value='NA'>Not Applicable</option>
               {departmentsData.departments.map((department: DepartmentsProps, index: any) =>
                 <option value={department.name} key={index}>{department.name}</option>
               )}
             </Select>
-            {/* <Input variant='filled' placeholder='Message' bg={containerColour} boxShadow='md' w={inputContainerWidth}/> */}
+
+            <FormControl isRequired>
+              <Textarea name='Message' variant='filled' placeholder='Message' rows={7} bg={containerColour} boxShadow='md' w={inputContainerWidth} />
+            </FormControl>
           </Stack>
-          {/* <div className="mb-5 w-[280px] md:w-[480px] lg:w-[600px]">
-            <label htmlFor="name" className="block font-bold" />
-            <input
-              name="name"
-              type="text"
-              required
-              placeholder="Full Name"
-              className="border-1 w-full rounded-md p-2 bg-[#f5f5f7] shadow-md text-base md:text-lg h-[30px] md:h-[40px]"
-            />
-          </div> */}
 
-          {/* <div className="mb-5 w-[280px] md:w-[480px] lg:w-[600px]">
-            <label htmlFor="email" className="block font-bold" />
-            <input
-              name="email"
-              type="email"
-              required
-              placeholder="Email Address"
-              className="border-1 w-full rounded-md p-2 bg-[#f5f5f7] shadow-md text-base md:text-lg h-[30px] md:h-[40px]"
-            />
-          </div> */}
-
-          {/* <div className="my-5">
-            <Select variant='filled' placeholder='Role' bg={containerColour} boxShadow='md' w={inputContainerWidth} h={inputContainerHeight}>
-              <option value='NA'>Not Applicable</option>
-              {rolesData.roles.map((role: RoleProps, index: any) =>
-                <option value={role.name} key={index}>{role.name}</option>
-              )}
-            </Select>
-          </div>
-
-          <div className="mb-5">
-            <Select variant='filled' placeholder='Relevant Department' bg={containerColour} boxShadow='md' w={inputContainerWidth} h={inputContainerHeight}>
-              <option value='NA'>Not Applicable</option>
-              {departmentsData.departments.map((department: DepartmentsProps, index: any) =>
-                <option value={department.name} key={index}>{department.name}</option>
-              )}
-            </Select>
-          </div> */}
-
-          {/* message */}
-          <div className="mb-5">
-            <label htmlFor="message" className="block font-bold" />
-            <textarea
-              name="message"
-              required
-              rows={5}
-              placeholder="Message"
-              className="border-1 w-full rounded-md p-2 bg-[#f5f5f7] shadow-md text-base md:text-lg"
-            />
-          </div>
           <div className="flex justify-center">
             <button
               type="submit"
@@ -98,12 +106,6 @@ const Form = () => {
               Send Message
             </button>
           </div>
-
-
-          {/* <input
-            type="submit"
-            className="md:mt-5 flex h-[35px] w-[150px] md:h-[55px] md:w-[275px] items-center justify-center rounded-[15px] md:rounded-[20px] bg-[#004080] text-base md:text-xl font-semibold text-white"
-          /> */}
         </form>
 
       </div>
