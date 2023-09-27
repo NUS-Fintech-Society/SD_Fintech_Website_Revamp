33c91837fb519dfe18f7f4a17bf63f9b21133d52
kwuunnn <e0958174@u.nus.edu>
2023-09-27 16:50:37 +0800

    footer location

--- components/layout/MaxWidth.tsx
+++ components/layout/MaxWidth.tsx
@@ -2,7 +2,9 @@ import { MaxWidthProps } from '@interfaces/layout/MaxWidth';
 import React from 'react';
 
 const MaxWidth = ({ children }: MaxWidthProps) => {
-  return <div className="mx-auto max-w-main px-6 sm:px-28">{children}</div>;
+  return (
+    <div className="mx-auto flex max-w-main px-6 sm:px-28">{children}</div>
+  );
 };
 
 export default MaxWidth;
