76bee232b1600ee758dc142e96e65ea037e343bd
Shelia Teo <sheliateo@Shelias-MacBook-Pro.local>
2023-09-29 12:07:02 +0800

    Replace Direct Message with Email Hyperlink

--- components/contact/ContactForm.tsx
+++ components/contact/ContactForm.tsx
@@ -1,23 +1,33 @@
 // code
 import Info from '@components/contact/Info';
 import MaxWidth from '@components/layout/MaxWidth';
-import Form from '@components/contact/Form';
 
 const ContactForm = () => {
   return (
     <MaxWidth>
-      <div className="section-my flex w-full flex-wrap text-lg md:text-xl lg:justify-around">
-        <div className="mb-10 w-full lg:order-2 lg:flex lg:w-1/2">
-          <div className="lg:w-5/6">
-            <Info />
-          </div>
-        </div>
-        <div className="w-full lg:order-1 lg:w-1/2">
-          <div className="lg:w-5/6">
-            <Form />
-          </div>
-        </div>
+      <div className="m-2 flex flex-col items-center md:m-5 lg:mt-0">
+        <h1 className="mt-2 w-full self-center text-center text-base font-bold text-[#004080] md:text-2xl lg:mt-4 lg:w-full lg:text-3xl">
+          Don’t find your question? Drop us an email below!
+        </h1>
+
+        <div className="mt-4 flex flex-col items-center">
+        <p className="text-center text-[#004080] md:text-lg lg:text-xl">
+          Internal Affairs and External Relations: <a href="mailto:nusfintech.ops@gmail.com">nusfintech.ops@gmail.com</a>
+        </p>
+        <p className="text-center text-[#004080] md:text-lg lg:text-xl">  
+          Blockchain: <a href="mailto:nusfintech.bc@gmail.com">nusfintech.bc@gmail.com</a>
+        </p>
+        <p className="text-center text-[#004080] md:text-lg lg:text-xl"> 
+          Quant: <a href="mailto:nusfintech.quant@gmail.com">nusfintech.quant@gmail.com</a> 
+        </p>
+        <p className="text-center text-[#004080] md:text-lg lg:text-xl"> 
+          Software Development: <a href="mailto:nusfintech.dev@gmail.com">nusfintech.dev@gmail.com</a>
+        </p>
+        <p className="text-center text-[#004080] md:text-lg lg:text-xl"> 
+          Machine Learning: <a href="mailto:nusfintech.ml@gmail.com">nusfintech.ml@gmail.com</a>
+        </p>
       </div>
+    </div>
     </MaxWidth>
   );
 };
