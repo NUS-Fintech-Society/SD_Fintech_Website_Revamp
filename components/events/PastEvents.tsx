81feab14a693112f3c9eb83abd36c5caed6bc495
kwuunnn <e0958174@u.nus.edu>
2023-11-14 20:54:07 +0800

    added events carousel

--- components/events/PastEvents.tsx
+++ components/events/PastEvents.tsx
@@ -2,11 +2,56 @@ import EventsProps from '@interfaces/events/Events';
 import { Event } from '@data/interface/eventsData';
 import SectionHeader from '@components/layout/SectionHeader';
 import PastEventCard from './PastEventCard';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 1536, min: 1280 },
+    items: 4,
+    partialVisibilityGutter: 30,
+  },
+  desktop: {
+    breakpoint: { max: 1280, min: 1124 },
+    items: 3,
+    partialVisibilityGutter: 30,
+  },
+  smallDesktop: {
+    breakpoint: { max: 1124, min: 1000 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1000, min: 810 },
+    items: 2,
+    partialVisibilityGutter: 30,
+  },
+  smallTablet: {
+    breakpoint: { max: 810, min: 750 },
+    items: 2,
+  },
+  mobileLandscape: {
+    breakpoint: { max: 750, min: 640 },
+    items: 1,
+    partialVisibilityGutter: 140,
+  },
+  mobile: {
+    breakpoint: { max: 640, min: 0 },
+    items: 1,
+  },
+};
 
 const PastEvents = ({ events }: EventsProps) => {
   const pastEvents: Event[] = events.filter(
     (a: Event) => new Date(a.date).getTime() <= new Date().getTime()
   );
+  const eventChunks = [];
+  const chunkSize = 2;
+
+  for (let i = 0; i < pastEvents.length; i += chunkSize) {
+    const chunk = pastEvents.slice(i, i + chunkSize);
+    eventChunks.push(chunk);
+  }
+
   return (
     <div className="sm:section-my mt-10">
       <SectionHeader
@@ -14,25 +59,75 @@ const PastEvents = ({ events }: EventsProps) => {
         title="Past Events"
         subtitle="A walk down memory lane."
       />
-      <div className="mt-10 grid w-full grid-flow-col grid-rows-2 place-content-start gap-1 overflow-hidden sm:mt-16 sm:gap-4">
-        {pastEvents.map(
-          (
-            { coverImage, name, cardDescription, location, date, type },
-            index
-          ) => {
-            return (
-              <PastEventCard
-                key={index}
-                coverImage={coverImage}
-                name={name}
-                cardDescription={cardDescription}
-                location={location}
-                date={date}
-                type={type}
-              />
-            );
-          }
-        )}
+      {/* Mobile Carousel */}
+      <div className="sm:hidden">
+        <Carousel
+          swipeable={true}
+          draggable={true}
+          showDots={true}
+          responsive={responsive}
+          ssr={false} // means to not render carousel on server-side.
+          infinite={true}
+          // autoPlay={true}
+          // autoPlaySpeed={3000}
+          keyBoardControl={true}
+          partialVisible={true}
+        >
+          {pastEvents.map(
+            (
+              { coverImage, name, cardDescription, location, date, type },
+              cardIndex
+            ) => (
+              <div key={cardIndex} className="my-7 flex justify-center">
+                <PastEventCard
+                  coverImage={coverImage}
+                  name={name}
+                  cardDescription={cardDescription}
+                  location={location}
+                  date={date}
+                  type={type}
+                />
+              </div>
+            )
+          )}
+        </Carousel>
+      </div>
+      {/* Desktop Carousel */}
+      <div className="hidden sm:block">
+        <Carousel
+          swipeable={true}
+          draggable={true}
+          showDots={true}
+          responsive={responsive}
+          ssr={false} // means to not render carousel on server-side.
+          infinite={false}
+          // autoPlay={true}
+          // autoPlaySpeed={3000}
+          keyBoardControl={true}
+          partialVisible={true}
+        >
+          {eventChunks.map((chunk, index) => (
+            <div key={index}>
+              {chunk.map(
+                (
+                  { coverImage, name, cardDescription, location, date, type },
+                  cardIndex
+                ) => (
+                  <div key={cardIndex} className="my-7 flex justify-center">
+                    <PastEventCard
+                      coverImage={coverImage}
+                      name={name}
+                      cardDescription={cardDescription}
+                      location={location}
+                      date={date}
+                      type={type}
+                    />
+                  </div>
+                )
+              )}
+            </div>
+          ))}
+        </Carousel>
       </div>
     </div>
   );
--- components/events/UpcomingEvents.tsx
+++ components/events/UpcomingEvents.tsx
@@ -4,6 +4,43 @@ import SectionHeader from '@components/layout/SectionHeader';
 import UpcomingEventCard from './UpcomingEventCard';
 import { useState } from 'react';
 import { Select } from '@chakra-ui/react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 1536, min: 1280 },
+    items: 4,
+    partialVisibilityGutter: 30,
+  },
+  desktop: {
+    breakpoint: { max: 1280, min: 1124 },
+    items: 3,
+    partialVisibilityGutter: 30,
+  },
+  smallDesktop: {
+    breakpoint: { max: 1124, min: 1000 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1000, min: 810 },
+    items: 2,
+    partialVisibilityGutter: 30,
+  },
+  smallTablet: {
+    breakpoint: { max: 810, min: 750 },
+    items: 2,
+  },
+  mobileLandscape: {
+    breakpoint: { max: 750, min: 640 },
+    items: 1,
+    partialVisibilityGutter: 140,
+  },
+  mobile: {
+    breakpoint: { max: 640, min: 0 },
+    items: 1,
+  },
+};
 
 const UpcomingEvents = ({ events }: EventsProps) => {
   const upcomingEvents: Event[] = events.filter(
@@ -16,6 +53,14 @@ const UpcomingEvents = ({ events }: EventsProps) => {
     selectedEventType === 'All'
       ? upcomingEvents
       : upcomingEvents.filter((event) => event.type === selectedEventType);
+
+  const eventChunks = [];
+  const chunkSize = 2;
+
+  for (let i = 0; i < filteredEvents.length; i += chunkSize) {
+    const chunk = filteredEvents.slice(i, i + chunkSize);
+    eventChunks.push(chunk);
+  }
 
   return (
     <div className="sm:section-my mt-10">
@@ -40,26 +85,85 @@ const UpcomingEvents = ({ events }: EventsProps) => {
         <option value="External">External</option>
       </Select>
       {filteredEvents.length > 0 ? (
-        <div className="grid w-full grid-flow-col grid-rows-2 place-content-start gap-1 overflow-hidden sm:gap-4">
-          {filteredEvents.map(
-            (
-              { coverImage, name, cardDescription, location, date, type },
-              index
-            ) => {
-              return (
-                <UpcomingEventCard
-                  key={index}
-                  coverImage={coverImage}
-                  name={name}
-                  cardDescription={cardDescription}
-                  location={location}
-                  date={date}
-                  type={type}
-                />
-              );
-            }
-          )}
-        </div>
+        <>
+          {/* Mobile Carousel */}
+          <div className="sm:hidden">
+            <Carousel
+              swipeable={true}
+              draggable={true}
+              showDots={true}
+              responsive={responsive}
+              ssr={false} // means to not render carousel on server-side.
+              infinite={true}
+              // autoPlay={true}
+              // autoPlaySpeed={3000}
+              keyBoardControl={true}
+              partialVisible={true}
+            >
+              {filteredEvents.map(
+                (
+                  { coverImage, name, cardDescription, location, date, type },
+                  cardIndex
+                ) => (
+                  <div key={cardIndex} className="mb-7 flex justify-center">
+                    <UpcomingEventCard
+                      coverImage={coverImage}
+                      name={name}
+                      cardDescription={cardDescription}
+                      location={location}
+                      date={date}
+                      type={type}
+                    />
+                  </div>
+                )
+              )}
+            </Carousel>
+          </div>
+          {/* Desktop Carousel */}
+          <div className="hidden sm:block">
+            <Carousel
+              swipeable={true}
+              draggable={true}
+              showDots={true}
+              responsive={responsive}
+              ssr={false} // means to not render carousel on server-side.
+              infinite={true}
+              // autoPlay={true}
+              // autoPlaySpeed={3000}
+              keyBoardControl={true}
+              partialVisible={true}
+            >
+              {eventChunks.map((chunk, index) => (
+                <div key={index}>
+                  {chunk.map(
+                    (
+                      {
+                        coverImage,
+                        name,
+                        cardDescription,
+                        location,
+                        date,
+                        type,
+                      },
+                      cardIndex
+                    ) => (
+                      <div key={cardIndex} className="mb-7 flex justify-center">
+                        <UpcomingEventCard
+                          coverImage={coverImage}
+                          name={name}
+                          cardDescription={cardDescription}
+                          location={location}
+                          date={date}
+                          type={type}
+                        />
+                      </div>
+                    )
+                  )}
+                </div>
+              ))}
+            </Carousel>
+          </div>
+        </>
       ) : (
         <div className="flex items-center justify-center">
           <div className="mt-2 text-base md:mt-4 md:text-2xl lg:mt-6 lg:text-3xl">
