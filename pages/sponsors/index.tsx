47294ded54e1ad3bfafb4853e7dbf011df7180be
Shelia Teo <teo.zihui.shelia@gmail.com>
2024-01-31 18:03:37 +0800

    added sponsors route

---
+++ pages/sponsors/index.tsx
@@ -0,0 +1,78 @@
+import { useBreakpointValue } from '@chakra-ui/react';
+import React from 'react';
+import Image from 'next/image';
+import { HeaderContainer, RowContainer, SponsorWrapper } from './styles';
+import { Organization } from '../../components/types/Organization';
+import { Gold, Organisers, Platinum, Silver } from '../../data/organization';
+
+export default function Sponsor() {
+  
+  function organizationItem(organization: Organization) {
+    return (
+      <a
+        href={organization.url}
+        target="_blank"
+        rel="noreferrer"
+        key={organization.name}
+        className={`${
+          organization.name === 'Tokka Labs' ? 'bg-[#0C1747]' : 'bg-white'
+        } rounded-lg shadow-lg px-4 py-2 w-fit h-24 flex justify-center items-center`}
+      >
+        <Image
+          className="scale-75"
+          src={organization.logoSrc}
+          alt={organization.name}
+          height={organization.logoHeight}
+          width={organization.logoWidth}
+        />
+      </a>
+    );
+  }
+  function organizationRow(organizations: Organization[], index: number) {
+    const row = organizations.map((organization) =>
+      organizationItem(organization),
+    );
+
+    return (
+      <RowContainer spacing="34" key={index}>
+        {row}
+      </RowContainer>
+    );
+  }
+  const rowSize = useBreakpointValue({ base: 2, sm: 1, md: 2, lg: 2 });
+
+  function organizationSection(
+    organizations: Organization[],
+    rowLength: number,
+  ) {
+    const result = [];
+
+    for (let i = 0; i < organizations.length; i += rowLength) {
+      result.push(organizations.slice(i, i + rowLength));
+    }
+
+    return (
+      <div className="flex flex-col gap-6">
+        {result.map((row, index) => organizationRow(row, index))}
+      </div>
+    );
+  }
+
+  return (
+    <SponsorWrapper overflow="hidden">
+      <HeaderContainer>Sponsors</HeaderContainer>
+
+      <HeaderContainer fontWeight="normal">Organized By</HeaderContainer>
+      {organizationSection(Organisers, rowSize || 2)}
+
+      <HeaderContainer fontWeight="normal">Platinum</HeaderContainer>
+      {organizationSection(Platinum, rowSize || 2)}
+
+      <HeaderContainer fontWeight="normal">Gold</HeaderContainer>
+      {organizationSection(Gold, rowSize || 2)}
+
+      <HeaderContainer fontWeight="normal">Silver</HeaderContainer>
+      {organizationSection(Silver, rowSize || 2)}
+    </SponsorWrapper>
+  );
+}
---
+++ pages/sponsors/styles.tsx
@@ -0,0 +1,36 @@
+import { Box, chakra, HStack } from '@chakra-ui/react';
+
+export const SponsorWrapper = chakra(Box, {
+  baseStyle: {
+    width: '100%',
+    justifyContent: 'center',
+    backgroundColor: '#20345b',
+    paddingTop: '34px',
+    paddingX: '34px',
+  },
+});
+
+export const RowContainer = chakra(HStack, {
+  baseStyle: {
+    h: '75px',
+    justifyContent: 'center',
+    paddingBottom: '100px',
+  },
+});
+
+export const HeaderContainer = chakra(Box, {
+  baseStyle: {
+    display: 'flex',
+    fontWeight: 'bold',
+    fontSize: '30px',
+    justifyContent: 'center',
+    paddingBottom: '70px',
+    color: 'white',
+  },
+  sizes: {
+    lg: {
+      paddingTop: '100px',
+      paddingBottom: '100px',
+    },
+  },
+});
